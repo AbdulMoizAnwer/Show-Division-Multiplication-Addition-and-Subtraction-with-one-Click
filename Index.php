9fb672e6c650c58fbb46d59ed3c8cff6f8d3d2ff
AbdulMoizAnwer <89318559+AbdulMoizAnwer@users.noreply.github.com>
2022-12-31 15:50:05 +0500

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Lecture 1</title>
+</head>
+<body>
+    <form action="backend.php" method="GET">
+        <label>Enter First Number</label>
+        <input type="number" name="n1"><br>
+        <label>Enter Second Number</label>
+        <input type="number" name="n2"><br>
+        <input type="submit" name="submit">
+    </form>
+</body>
+</html>
